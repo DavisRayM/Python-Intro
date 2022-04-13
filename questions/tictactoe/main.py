c4c2c2f13d508316100381f766d5867d0a5f133b
Diana Mutheu <dianamulyungi@gmail.com>
2022-04-13 12:18:58 +0300

    added players functionality

--- questions/tictactoe/main.py
+++ questions/tictactoe/main.py
@@ -9,118 +9,79 @@ from typing import List
 
 
 class TicTacToe:
-    player_a = "X"
-    player_b = "O"
-
-
     def __init__(self):
-        # self.position_map = {1:(0,0),2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2),7:(2,0),8:(2,1),9:(2,2)}
-        self.board = []
-    def build_initial_board(self) -> List[list]:
-        """
-        This function is used to generate the initial tic
-        tac toe board which is a 3 * 3 board
-        -------------
-        |   |   |   |
-        -------------
-        |   |   |   |
-        -------------
-        |   |   |   |
-        -------------
-        """
+        self._build_intial_board()
+        self.current_round = 0
+        self.position_map = {1:(0,0),2:(0,1),3:(0,2),4:(1,0),5:(1,1),6:(1,2),7:(2,0),8:(2,1),9:(2,2)}
+
+
+    def _build_initial_board(self):
 
         self.board = [["-","-","-"],["-","-","-"],["-","-","-"]]
-        print(self.board)
 
     def print_board(self):
         for row in self.board:
             print(" | ".join(row))
         pass
 
-    def place_token(self):
-        if user_input > 9 or user_input  < 1 or type(user_input) != int:
-            print("Please enter a valid number betweeb 1 and 9 ")
-        #import pdb; pdb.set_trace()
+    def _validate_move(self,cell_no):
+        #checks if its a valid number
+        if cell_no > 9 or cell_no<1:
+            raise ValueError("Please enter a valid number between 1 to 9")
+        x, y = self.position_map.get(cell_no)
+        #checks if cell is occupied
+        if self.board[x][y] != "-":
+            raise ValueError(f"Cell is already occupied by player {self.board[x][y]}")
 
-        self.board[0][0] == 0
-        self.board[0][1] == 1
-        self.board[0][2] == 2
-        self.board[1][0] == 3
-        self.board[1][1] == 4
-        self.board[0][2] == 5
-        self.board[2][0] == 6
-        self.board[2][1] == 7
-        self.board[2][2] == 8
-         # x,y = self.position_map[user_input]#same way as indexing
-
-        # if self.board[x][y] == '-':
-        #     self.board[x][y] = 'x'
-            # if x is has played now its 'o' turn.
-    pass
-
-    def swap_user(self):
-        for row in self.board:
-            for i in row:
-                    if self.board[0][0] == '-':
-                        self.board[0][0] = 'player_a'
-
-                    elif self.board[0][2] == '-':
-                        self.board[0][2] = 'player_a'
-
-                    elif self.board[1][1] == '-':
-                        self.board[1][1] = 'player_a'
-
-                    elif self.board[2][0] == '-':
-                        self.board[2][0] = 'player_a'
-
-                    elif self.board[2][2] == '-':
-                        self.board[2][2] = 'player_a'
-
-                    elif self.board[0][1] == '-':
-                        self.board[0][1] = 'player_b'
-
-                    elif self.board[1][0] == '-':
-                        self.board[1][0] = 'player_b'
-
-                    elif self.board[1][2] == '-':
-                        self.board[1][2] = 'player_b'
-
-                    elif self.board[2][1] == '-':
-                        self.board[2][1] = 'player_b'
-                    else:
-                        print("Invalid choice")
-
-
-    def winner(self):
-        # checking winner
-        if self.board[0][0] == self.board[0][1] == self.board[0][2] != "-" :
-                print("Game over!")
-        elif self.board[1][0] == self.board[1][1] == self.board[1][2] != "-" :
-                print("Game over!")
-        elif self.board[2][0] == self.board[2][1] == self.board[2][2] != "-":
-                print("Game over!")
-        elif self.board[0][0] == self.board[1][0] == self.board[2][0] != "-":
-                print("Game over!")
-        elif self.board[0][1] == self.board[1][1] == self.board[2][1] != "-":
-            print("Game over!")
-        elif self.board[0][2] == self.board[1][2] == self.board[2][2] != "-":
-                print("Game over!")
-        elif self.board[0][0] == self.board[1][1] == self.board[2][2] != "-":
-                print("Game over!")
-        elif self.board[0][2] == self.board[1][1] == self.board[2][2] != "-":
-                print("Game over!")
+    def _get_current_user(self):
+        if self.current_round %2 == 0 :
+            return'o'
         else:
-            print("Try again")
+            return 'x'
+    def place_token(self,cell_no):
+        self._validate_move(cell_no)
+        x,y =self.position_map.get(cell_no)
+        current_player = self._get_current_user()
+        self.board[x][y] = current_player
+        self.print_board()
+
+    def check_win(self):
+        if self.board[0][0] == self.board[0][1] == self.board[0][2] and self.board[0][0] != "-" :
+                return True
+        elif self.board[1][0] == self.board[1][1] == self.board[1][2] and self.board[1][0] != "-" :
+                return True
+        elif self.board[2][0] == self.board[2][1] == self.board[2][2] and self.board[2][0] != "-":
+                return True
+        elif self.board[0][0] == self.board[1][0] == self.board[2][0] and self.board[0][0] != "-":
+                return True
+        elif self.board[0][1] == self.board[1][1] == self.board[2][1] and self.board[0][1] != "-":
+            return True
+        elif self.board[0][2] == self.board[1][2] == self.board[2][2] and self.board[0][2] != "-":
+                return True
+        elif self.board[0][0] == self.board[1][1] == self.board[2][2] and self.board[0][0] != "-":
+                return True
+        elif self.board[0][2] == self.board[1][1] == self.board[2][2] and self.board[0][2] != "-":
+                return True
+        else:
+            return False
 
     def play_game(self):
+        game_ongoing = True
+        while game_ongoing:
+            self.current_round == 1
+            current_player = self._get_current_user()
+            try:
+                user_input= input(f"Player{current_player}, Enter a number between 1 to 9:")
+                self.place_token(int(user_input))
+            except ValueError as e:
+                print(e)
+                user_input = input(f"Player{current_player}, Enter a number between 1 to 9: ")
+                self.place_token(int(user_input))
 
-        pass
+
+                if self.check_win():
+                    print(f"Player {current_player} wins! Game Over")
+                    game_ongoing = False
 
 if __name__ == "__main__":
-    user_input = int(input("Enter a number between 1 to 9 "))
-    user_input -= 1
-    board = TicTacToe()
-    print(board.build_initial_board())
-    print(board.print_board())
-    print(board.place_token())
-
+    TicTacToe().play_game()
